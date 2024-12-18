59c866f1be7a172eb9f14b4bb99cbae8cc1c8d43
Dede <dbangtan38@gmail.com>
2024-12-18 11:17:47 +0700

    sampe utils

---
+++ rencana_receh/lib/core/utils/image_constant.dart
@@ -0,0 +1,42 @@
+//ignore_for_file: must_be_immutable
+class ImageConstant {
+// Image folder path
+  static String imagePath = 'assets/images';
+// Common images
+  static String imgArrowLeft = '$imagePath/img_arrow_left.svg';
+  static String imgEyeOff = '$imagePath/img_eye_off.svg';
+  static String imgGroup94 = '$imagePath/img_group_94.png';
+  static String imgGroup93 = '$imagePath/img_group_93.png';
+  static String imgFingerprint = '$imagePath/img_fingerprint.svg';
+  static String imgCheckmark = '$imagePath/img_checkmark.svg';
+  static String imgUser = '$imagePath/img_user.svg';
+  static String imgArrowLeftBlueGray900 =
+      '$imagePath/img_arrow_left_blue_gray_900.svg';
+
+  static String imgIcon = '$imagePath/img_icon.svg';
+  static String imgIconTea1200 = '$imagePath/img_icon_teal_200.svg';
+  static String imgIcon34 = '$imagePath/img_icon_34.svg';
+  static String imgGroup268 = '$imagePath/img_group_268.svg';
+  static String imgIconCategory = '$imagePath/img_icon_category.svg';
+  static String imgIconCategoryPinkA200 =
+      '$imagePath/img_icon_category_pink_a200.svg';
+  static String imgIconCategoryLightBlueA700 =
+      '$imagePath/img_icon_category_light_blue_a700.svg';
+  static String imgIconCategoryOrange400 =
+      '$imagePath/img_icon_category_orange_400.svg';
+  static String imgIconCategoryTea1300 =
+      '$imagePath/img_icon_category_teal_300.svg';
+  static String imgThumbsUp = '$imagePath/img_thumbs_up.svg';
+
+  static String imgTelevision = '$imagePath/img_television.svg';
+  static String imgIconCategoryPrimary =
+      '$imagePath/img_icon_category_primary.svg';
+  static String imgUserPinkA200 = '$imagePath/img_user_pink_a200.svg';
+  static String imgNavHome = '$imagePath/img_nav_home.svg';
+  static String imgClock = '$imagePath/img_clock.svg';
+  static String imgLock = '$imagePath/img_lock.svg';
+  static String imgIcon22 = '$imagePath/img_icon_22.svg';
+  static String imgImage4 = '$imagePath/img_image_4.png';
+  static String imgIconsEAdd2 = '$imagePath/img_icons_e_add_2.svg';
+  static String imageNotFound = 'assets/images/image_not_found.png';
+}
